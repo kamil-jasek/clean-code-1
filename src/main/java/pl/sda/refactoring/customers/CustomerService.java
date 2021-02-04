809b321318626315c2b710b6197665ed30e881ff
Kamil Jasek <kamil.jasek@dbschenker.com>
2021-02-04 12:14:37 +0100

    stage-4

--- src/main/java/pl/sda/refactoring/customers/CustomerService.java
+++ src/main/java/pl/sda/refactoring/customers/CustomerService.java
@@ -11,23 +11,14 @@ public class CustomerService {
     private static final Pattern EMAIL_PATTERN = Pattern.compile(
         "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
 
-    private CustomerDao dao;
-    private MailSender mailSender;
+    private final CustomerDao dao;
+    private final MailSender mailSender;
 
     public CustomerService(CustomerDao dao, MailSender mailSender) {
         this.dao = requireNonNull(dao);
         this.mailSender = requireNonNull(mailSender);
     }
 
-    /**
-     * Register new person type customer
-     * @param email
-     * @param firstName
-     * @param lastName
-     * @param pesel
-     * @param verified
-     * @return
-     */
     public boolean registerPerson(String email, String firstName, String lastName, String pesel, boolean verified) {
         var result = false;
         var customer = new Customer();
@@ -75,7 +66,7 @@ public class CustomerService {
     }
 
     private boolean matchesPesel(String pesel) {
-        return pesel.matches("/\\d{11}/");
+        return pesel.matches("\\d{11}");
     }
 
     private boolean matchesEmail(String email) {
--- src/test/java/pl/sda/refactoring/customers/CustomerServiceTest.java
+++ src/test/java/pl/sda/refactoring/customers/CustomerServiceTest.java
@@ -1,9 +1,14 @@
 package pl.sda.refactoring.customers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
 class CustomerServiceTest {
 
@@ -19,4 +24,29 @@ class CustomerServiceTest {
         // then
         assertFalse(result);
     }
+
+    @Test
+    void shouldRegisterNotVerifiedPerson() {
+        // when
+        final var result = service.registerPerson("em@test.com", "Jan", "Kowalski", "92893202093", false);
+
+        // then
+        final var customer = verifyCustomerSaved();
+        assertTrue(result);
+        assertEquals(Customer.PERSON, customer.getType());
+        assertNotNull(customer.getId());
+        assertFalse(customer.isVerf());
+        assertEquals("em@test.com", customer.getEmail());
+        assertEquals("Jan", customer.getfName());
+        assertEquals("Kowalski", customer.getlName());
+        assertEquals("92893202093", customer.getPesel());
+        assertNotNull(customer.getCtime());
+    }
+
+    private Customer verifyCustomerSaved() {
+        final var captor = ArgumentCaptor.forClass(Customer.class);
+        verify(dao).save(captor.capture());
+        return captor.getValue();
+    }
+
 }
