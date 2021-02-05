0c8428b91f36bfc40a5cabfcdc2032167e17d018
Kamil Jasek <kamil.jasek@dbschenker.com>
2021-02-05 13:46:06 +0100

    stage-16

--- src/test/java/pl/sda/refactoring/customers/EventPublisherTest.java
+++ src/test/java/pl/sda/refactoring/customers/EventPublisherTest.java
@@ -39,4 +39,60 @@ class EventPublisherTest {
         assertEquals("<b>Hi Jan</b><br/>Thank you for registering in our service. Now you are verified customer!",
             mailSender.getBody());
     }
+
+    @Test
+    void shouldSendNotVerifiedMailAboutRegisteredPerson() {
+        // when
+        publisher.publish(new RegisteredPersonEvent(
+            UUID.randomUUID(),
+            "em@test.com",
+            LocalDateTime.now(),
+            "Jan",
+            "Kowalski",
+            "93939939222",
+            null));
+
+        // then
+        assertEquals("em@test.com", mailSender.getRecipient());
+        assertEquals("Waiting for verification", mailSender.getSubject());
+        assertEquals("<b>Hi Jan</b><br/>We registered you in our service. Please wait for verification!",
+            mailSender.getBody());
+    }
+
+    @Test
+    void shouldSendVerifiedMailAboutRegisteredCompany() {
+        // when
+        publisher.publish(new RegisteredCompanyEvent(
+            UUID.randomUUID(),
+            "em@test.com",
+            LocalDateTime.now(),
+            "test",
+            "93839402033",
+            new CustomerVerification(LocalDateTime.now(), CustomerVerifier.AUTO_EMAIL)));
+
+        // then
+        assertEquals("em@test.com", mailSender.getRecipient());
+        assertEquals("Your are now verified customer!", mailSender.getSubject());
+        assertEquals("<b>Your company: test is ready to make na order.</b><br/>"
+                + "Thank you for registering in our service. Now you are verified customer!",
+            mailSender.getBody());
+    }
+
+    @Test
+    void shouldSendNotVerifiedMailAboutRegisteredCompany() {
+        // when
+        publisher.publish(new RegisteredCompanyEvent(
+            UUID.randomUUID(),
+            "em@test.com",
+            LocalDateTime.now(),
+            "test",
+            "93839402033",
+            null));
+
+        // then
+        assertEquals("em@test.com", mailSender.getRecipient());
+        assertEquals("Waiting for verification", mailSender.getSubject());
+        assertEquals("<b>Hello</b><br/>We registered your company: test in our service. Please wait for verification!",
+            mailSender.getBody());
+    }
 }
